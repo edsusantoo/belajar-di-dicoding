9b3f3f0acf617dc958bc96750ce0adfe5dd4fad5
Edi Susanto <edsusantoo@gmail.com>
2019-08-13 12:30:55 +0700

    update when

--- android-starter-pack/AndroidJetpackPro/Codelab/codelab-dicoding-myunittesting/app/src/main/java/com/edsusantoo/bismillah/myunittesting/MainActivity.kt
+++ android-starter-pack/AndroidJetpackPro/Codelab/codelab-dicoding-myunittesting/app/src/main/java/com/edsusantoo/bismillah/myunittesting/MainActivity.kt
@@ -29,32 +29,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val length = edt_length.text.toString().trim()
         val width = edt_width.text.toString().trim()
         val height = edt_height.text.toString().trim()
-        if (TextUtils.isEmpty(length)) {
-            edt_length.error = "Field ini tidak boleh kosong"
-        } else if (TextUtils.isEmpty(width)) {
-            edt_width.error = "Field ini tidak boleh kosong"
-        } else if (TextUtils.isEmpty(height)) {
-            edt_height.error = "Field ini tidak boleh kosong"
-        } else {
-            val l = java.lang.Double.parseDouble(length)
-            val w = java.lang.Double.parseDouble(width)
-            val h = java.lang.Double.parseDouble(height)
-            when {
-                v?.id == R.id.btn_save -> {
-                    mainViewModel?.save(l, w, h)
-                    visible()
-                }
-                v?.id == R.id.btn_calculate_circumference -> {
-                    tv_result.text = (mainViewModel?.getCircumference().toString())
-                    gone()
-                }
-                v?.id == R.id.btn_calculate_surface_area -> {
-                    tv_result.text = mainViewModel?.getSurfaceArea().toString()
-                    gone()
-                }
-                v?.id == R.id.btn_calculate_volume -> {
-                    tv_result.text = mainViewModel?.getSurfaceArea().toString()
-                    gone()
+        when {
+            TextUtils.isEmpty(length) -> edt_length.error = "Field ini tidak boleh kosong"
+            TextUtils.isEmpty(width) -> edt_width.error = "Field ini tidak boleh kosong"
+            TextUtils.isEmpty(height) -> edt_height.error = "Field ini tidak boleh kosong"
+            else -> {
+                val l = java.lang.Double.parseDouble(length)
+                val w = java.lang.Double.parseDouble(width)
+                val h = java.lang.Double.parseDouble(height)
+                when {
+                    v?.id == R.id.btn_save -> {
+                        mainViewModel?.save(l, w, h)
+                        visible()
+                    }
+                    v?.id == R.id.btn_calculate_circumference -> {
+                        tv_result.text = (mainViewModel?.getCircumference().toString())
+                        gone()
+                    }
+                    v?.id == R.id.btn_calculate_surface_area -> {
+                        tv_result.text = mainViewModel?.getSurfaceArea().toString()
+                        gone()
+                    }
+                    v?.id == R.id.btn_calculate_volume -> {
+                        tv_result.text = mainViewModel?.getSurfaceArea().toString()
+                        gone()
+                    }
                 }
             }
         }
