93ceea4096c2ec4e8c1e3a987142893a66a79ba1
Edi Susanto <edsusantoo@gmail.com>
2019-08-13 08:58:47 +0700

    testing volume

---
+++ android-starter-pack/AndroidJetpackPro/Codelab/codelab-dicoding-myunittesting/app/src/test/java/com/edsusantoo/bismillah/myunittesting/MainViewModelTest.kt
@@ -0,0 +1,35 @@
+package com.edsusantoo.bismillah.myunittesting
+
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class MainViewModelTest {
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var cuboidModel: CuboidModel
+
+    private val dummyVolume = 504.0
+    private val dummyLength = 12.0
+    private val dummyWidth = 7.0
+    private val dummyHeight = 6.0
+
+    @Before
+    fun before() {
+        cuboidModel = mock(CuboidModel::class.java)
+        mainViewModel = MainViewModel(cuboidModel)
+    }
+
+    @Test
+    fun testVolume() {
+        cuboidModel = CuboidModel()
+        mainViewModel = MainViewModel(cuboidModel)
+
+        mainViewModel.save(dummyWidth, dummyLength, dummyHeight)
+
+        val volume: Double = mainViewModel.getVolume()
+
+        assertEquals(dummyVolume, volume, 0.0001)
+
+    }
+}
