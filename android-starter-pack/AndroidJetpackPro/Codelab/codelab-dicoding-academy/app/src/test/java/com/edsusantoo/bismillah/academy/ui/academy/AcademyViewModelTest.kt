5d05e5c267662a86e78028da1664bae716147eb2
Edi Susanto <edsusantoo@gmail.com>
2019-08-26 12:50:56 +0700

    testing live data (error)

--- android-starter-pack/AndroidJetpackPro/Codelab/codelab-dicoding-academy/app/src/test/java/com/edsusantoo/bismillah/academy/ui/academy/AcademyViewModelTest.kt
+++ android-starter-pack/AndroidJetpackPro/Codelab/codelab-dicoding-academy/app/src/test/java/com/edsusantoo/bismillah/academy/ui/academy/AcademyViewModelTest.kt
@@ -1,31 +1,49 @@
 package com.edsusantoo.bismillah.academy.ui.academy
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.edsusantoo.bismillah.academy.data.CourseEntity
 import com.edsusantoo.bismillah.academy.data.source.AcademyRepository
 import com.edsusantoo.bismillah.academy.utils.FakeDataDummyTest
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mock
 import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
+
 
 class AcademyViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule: InstantTaskExecutorRule = InstantTaskExecutorRule()
+
     private lateinit var viewModel: AcademyViewModel
     private var academyRepository: AcademyRepository = mock(AcademyRepository::class.java)
 
+
+    @Mock
+    lateinit var observeGetCourse: Observer<List<CourseEntity>>
+
     @Before
     fun setup() {
+        MockitoAnnotations.initMocks(this)
         viewModel = AcademyViewModel(academyRepository)
+        viewModel.getCourse()?.observeForever(observeGetCourse)
     }
+
 
     @Test
     fun getCourse() {
-        `when`(academyRepository.getAllCourses()).thenReturn(FakeDataDummyTest.generateDummyCourses())
 
-        val courseEntities: List<CourseEntity>? = viewModel.getCourse()
+        val dummyCourse: ArrayList<CourseEntity> = FakeDataDummyTest.generateDummyCourses()
 
-        verify(academyRepository).getAllCourses()
-        assertNotNull(courseEntities)
-        assertEquals(5, courseEntities?.size)
+        val courses: MutableLiveData<List<CourseEntity>> = MutableLiveData()
+        courses.value = dummyCourse
+
+        `when`(academyRepository.getAllCourses()).thenReturn(courses)
+
+        verify(observeGetCourse).onChanged(dummyCourse)
     }
 }
